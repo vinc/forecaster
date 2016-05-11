91a098f7d4cd5ccad8b6d93dc05eb991246672a5
Vincent Ollivier <v@vinc.cc>
2016-05-11 03:56:22 +0000

    Refactor configuration

--- lib/forecaster/configuration.rb
+++ lib/forecaster/configuration.rb
@@ -7,12 +7,17 @@ module Forecaster
       @cache_dir = "/tmp/forecaster"
       @curl_path = "curl"
       @wgrib2_path = "wgrib2"
+
+      # See: http://www.nco.ncep.noaa.gov/pmb/products/gfs/gfs_upgrade/gfs.t06z.pgrb2.0p25.f006.shtml
+      # See: http://www.cpc.ncep.noaa.gov/products/wesley/wgrib2/
+      # Use `variable` and `level` attributes separated by colons to identify
+      # the records to download and read.
       @records = {
-        prate: "PRATE:surface",
-        tmp:   "TMP:2 m above ground",
-        ugrd:  "UGRD:10 m above ground",
-        vgrd:  "VGRD:10 m above ground",
-        tcdc:  "TCDC:entire atmosphere"
+        :prate => ":PRATE:surface:",
+        :tmp   => ":TMP:2 m above ground:",
+        :ugrd  => ":UGRD:10 m above ground:",
+        :vgrd  => ":VGRD:10 m above ground:",
+        :tcdc  => ":TCDC:entire atmosphere:"
       }
     end
 
